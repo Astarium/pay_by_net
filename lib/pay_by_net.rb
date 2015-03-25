8d557ffa5aa7aae9a08c09345336e8196935b7ff
Aleksander Dąbrowski <tjeden@gmail.com>
2015-03-25 16:11:45 +0100

    init commit

---
+++ lib/pay_by_net.rb
@@ -0,0 +1,4 @@
+require 'pay_by_net/payment'
+
+module PayByNet
+end
---
+++ lib/pay_by_net/payment.rb
@@ -0,0 +1,8 @@
+module PayByNet
+  class Payment
+    attr_accessor :id_client, :id_trans, :date_valid, :amount, :currency, :email, :account, :accname, :backpage, :backpagereject, automat:
+
+    def initialize
+    end
+  end
+end
